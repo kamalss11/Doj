332b874bc3563d397e4d3a1f7d842b45667c6363
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-07-24 21:26:29 +0530

    Add files via upload

--- JS/js.js
+++ JS/js.js
@@ -1,9 +1,11 @@
 var img1 = document.querySelector(".pg-2 img")
-if(window.innerWidth <= 540){
-    img1.src = "Images/img2_mobile.png"
-    console.log(img1)
-}
-else{
-    img1.src = "Images/dojo_img1.png"
-    console.log(img1)
-}
+setInterval(function(){
+    if(window.innerWidth <= 540){
+        img1.src = "Images/img2_mobile.png"
+        console.log(img1)
+    }
+    else{
+        img1.src = "Images/dojo_img1.png"
+        console.log(img1)
+    }
+},500)
