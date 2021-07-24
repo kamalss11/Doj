18b1b26222f3dbb215ae17e641f8cfbedd634f74
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-07-24 20:56:16 +0530

    Add files via upload

---
+++ JS/js.js
@@ -0,0 +1,9 @@
+var img1 = document.querySelector(".pg-2 img")
+if(window.innerWidth <= 540){
+    img1.src = "Images/img2_mobile.png"
+    console.log(img1)
+}
+else{
+    img1.src = "Images/dojo_img1.png"
+    console.log(img1)
+}
